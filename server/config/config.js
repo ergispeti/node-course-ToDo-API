c6561ce9762c2dac44aaaaceca92564ae908d7f0
Stefano Poggi <Stefano Poggi>
2017-02-20 16:32:16 +0100

    Update 4

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.NODE_ENV || 'development';
 console.log('env ------ ', env);
 
 if(env === 'development'){
--- server/server.js
+++ server/server.js
@@ -56,30 +56,6 @@ app.get('/todos/:id', (req,res) => {
    }
 });
 
-app.post('/todos/:id', (req,res) => {
-  var id = req.params.id;
-
-   if (!ObjectID.isValid(id)){
-     return res.status(404).send();
-   }
-    ToDo.findById(id).then((todo) => {
-      if(!todo)
-      return res.status(404).send();
-
-      var todo = new ToDo({
-        text: req.body.text,
-        _id:  req.body._id,
-        completedAt: req.body.completedAt
-      });
-
-      todo.save().then((doc) => {
-        res.send(doc);
-      }, (e) => {
-        res.status(400).send(e);
-      });
-    })
-});
-
 app.delete('/todos/:id', (req,res) => {
   var id = req.params.id;
 
