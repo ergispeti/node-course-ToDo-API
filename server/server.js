744941c3d791c62c86bd4ae67d86ad85631f2040
Stefano Poggi <Stefano Poggi>
2017-02-20 15:29:52 +0100

    Add POST /todos/:id

--- server/server.js
+++ server/server.js
@@ -56,6 +56,32 @@ app.get('/todos/:id', (req,res) => {
    }
 });
 
+app.post('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+   if (!ObjectID.isValid(id)){
+     return res.status(404).send();
+   }
+   else{
+    ToDo.findById(id).then((todo) => {
+      if(!todo)
+      return res.status(404).send();
+
+      var todo = new ToDo({
+        text: req.body.text,
+        _id:  req.body._id,
+        completedAt = req
+      }).save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+      res.status(400).send();
+    }).catch((e) => {
+      res.status(400).send();
+    });
+   }
+ }
+});
+
 app.delete('/todos/:id', (req,res) => {
   var id = req.params.id;
 
